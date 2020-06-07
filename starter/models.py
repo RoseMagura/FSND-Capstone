75d7d1abe7dc4b05743e9694c4f71815b4fd38df
unknown <magurarm@guilford.edu>
2020-06-07 15:07:06 -0400

    Feat: Add helper functions to classes

--- starter/models.py
+++ starter/models.py
@@ -6,45 +6,29 @@ from flask import Flask
 from flask_migrate import Migrate
 import json
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1@localhost:5432/casting'
-migrate = Migrate(app, db)
-
+database_name = 'casting'
+database_path = 'postgresql://{}:{}@{}/{}'.format(
+                            'postgres', 1, 'localhost:5432', database_name)
 
 '''
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
-# def setup_db(app, database_path=database_path):
+def setup_db(app, database_path=database_path):
     
-    
-#     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-#     db.app = app
-#     db.init_app(app)
-#     db.create_all()
+    app = Flask(__name__)
+    db = SQLAlchemy(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+    migrate = Migrate(app, db)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
 
 cast = db.Table('cast',
     db.Column('actor_id', db.Integer, db.ForeignKey('Actors.id'), primary_key=True),
     db.Column('movie_id', db.Integer, db.ForeignKey('Movies.id'), primary_key=True))
-# class Cast(db.Model):
-#     __tablename__ = 'cast'
 
-#     actor_id = db.Column(db.Integer, db.ForeignKey('Actors.id'))
-#     movie_id = db.Column(db.Integer, db.ForeignKey('Movies.id')) 
-#     actor = db.relationship('Actor', back_populates=)
-
-#     def __init__(self, actor_id, movie_id):
-#         self.actor_id = actor_id
-#         self.movie_id = movie_id
-
-#     def format(self):
-#         return{
-#             'id': self.id,
-#             'actor_id': self.actor_id,
-#             'movie_id': self.movie_id
-#         } 
 '''
 Movie
 Attributes include title and release date
@@ -63,6 +47,17 @@ class Movie(db.Model):
         self.release_date = release_date
         self.actors = actors
 
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()        
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
     def format(self):
         return{
             'id': self.id,
@@ -70,6 +65,7 @@ class Movie(db.Model):
             'release_date': self.release_date, 
             'actors': [a.name for a in self.actors]
         }    
+
 
 '''
 Actor
@@ -90,6 +86,17 @@ class Actor(db.Model):
         self.age = age
         self.gender = gender
         self.movies = movies
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()        
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
     def format(self):
         return{
